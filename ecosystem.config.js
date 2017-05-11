a230be06bbc71ed2d5ed6e7b9694d03a87f92aac
AfterThreeYears <470326964@qq.com>
2017-05-11 16:45:28 +0800

    fix: 修改泡面

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,9 +9,9 @@ module.exports = {
     {
       name: 'ssr',
       script: 'server.js',
-      watch: true,
+      // watch: true,
       // instances: 4,
-      exec_mode: 'cluster',
+      // exec_mode: 'cluster',
       env_dev: {
         NODE_ENV: 'development',
       },
@@ -24,10 +24,10 @@ module.exports = {
     {
       name: 'mock',
       script: 'api/mock/index.js',
-      watch: true,
+      // watch: true,
       // instances: 4,
       interpreter_args: '--harmony-async-await',
-      exec_mode: 'cluster',
+      // exec_mode: 'cluster',
       env_dev: {
         NODE_ENV: 'development',
       },
@@ -41,25 +41,25 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy: {
-    production: {
-      user: 'node',
-      host: '212.83.163.1',
-      ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
-      path: '/var/www/production',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-    },
-    dev: {
-      user: 'node',
-      host: '212.83.163.1',
-      ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
-      path: '/var/www/development',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env: {
-        NODE_ENV: 'dev',
-      },
-    },
-  },
+  // deploy: {
+  //   production: {
+  //     user: 'node',
+  //     host: '212.83.163.1',
+  //     ref: 'origin/master',
+  //     repo: 'git@github.com:repo.git',
+  //     path: '/var/www/production',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+  //   },
+  //   dev: {
+  //     user: 'node',
+  //     host: '212.83.163.1',
+  //     ref: 'origin/master',
+  //     repo: 'git@github.com:repo.git',
+  //     path: '/var/www/development',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
+  //     env: {
+  //       NODE_ENV: 'dev',
+  //     },
+  //   },
+  // },
 };
